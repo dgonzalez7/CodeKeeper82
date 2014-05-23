d796432511a95167d8d0744b62d43495b7b1afb0
Dave Gonzalez <dgonzalez@thosegonzos.com>
2014-05-23 15:05:28 -0500

    Final version with "whacky" for loop.

--- CodeKeeper82/src/CodeKeeper.java
+++ CodeKeeper82/src/CodeKeeper.java
@@ -2,9 +2,45 @@ import java.util.*;
 
 public class CodeKeeper {
 
-	public static void main(String[] args) {
-		System.out.println("Projct 8.2");
-
+	ArrayList list;
+	String[] codes = { "alpha", "lambda", "gamma", "delta", "zeta" };
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Projct 8.2\n");
+		
+		CodeKeeper keeper = new CodeKeeper(args);
 	}
-
+	
+	public CodeKeeper(String[] userCodes)
+	{
+		list = new ArrayList();
+		// load built-in codes
+		for (int i = 0; i < codes.length; i++)
+		{
+			addCode(codes[i]);
+		}
+			
+		// load user codes
+		for (int j = 0; j < userCodes.length; j++)
+		{
+			addCode(userCodes[j]);
+		}
+		
+		// display all codes
+		//for (Iterator ite = list.iterator(); ite.hasNext(); )
+		for (Object output : list)
+		{
+			//String output = (String) ite.next();
+			System.out.println((String) output);
+		}
+	}
+		
+	private void addCode(String code)
+	{
+		if (!list.contains(code))
+		{
+			list.add(code);
+		}
+	}
 }
